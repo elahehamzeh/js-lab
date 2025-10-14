8b98f0eac2761fddb9ac1669aa7e2a056521bd31
elahehamzeh <zahrahamzeh69@yahoo.com>
2025-10-14 10:17:18 -0700

    create my project

---
+++ app.js
@@ -0,0 +1,21 @@
+
+// // فاکتوریل
+// const num=parseInt(prompt('emter number'));
+// let factorial=1;
+// for(let i=1;i<=num;i++){
+//     factorial*=i;
+//     console.log(factorial);
+// }
+
+// // خروجی ستاره
+// const nummber=parseInt(prompt('enter your number'));
+// for(let i=1;i<=nummber;i++){
+//     console.log("*".repeat(i));
+
+// }
+// // معکوس
+// const number=parseInt(prompt('enter a number'));
+// for(let i= number; i>=1;i--){
+//     console.log(i);
+// }
+    
