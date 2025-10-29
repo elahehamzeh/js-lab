274c3f3277a8940990c67d8481d0829a05ceb5e9
elahehamzeh <zahrahamzeh69@yahoo.com>
2025-10-29 23:44:13 +0330

    add practice files for 015

--- app.js
+++ app.js
@@ -18,4 +18,48 @@
 // for(let i= number; i>=1;i--){
 //     console.log(i);
 // }
-    
+
+
+// اسلایدر
+
+// const images = ["img.jpg", "imgg.webp", "imggg.jpg", "imgggg.webp"];
+// let index = 0;
+// let flag = true;
+
+// const slider = document.getElementById("slider");
+// const nextBtn = document.getElementById("next");
+// const prevBtn = document.getElementById("prev");
+
+// nextBtn.addEventListener("click", () => {
+//   flag = true;
+//   changeImage();
+// });
+
+// prevBtn.addEventListener("click", () => {
+//   flag = false;
+//   changeImage();
+// });
+
+// function changeImage() {
+//   if (flag === true) {
+//     index++;
+//     if (index >= images.length) index = 0;
+//   } else {
+//     index--;
+//     if (index < 0) index = images.length - 1;
+//   }
+//   slider.src = images[index];
+// }
+
+// عکس بزرگ رو انتخاب می‌کنیم
+const bigImage = document.getElementById('bigImage');
+
+
+const images = document.querySelectorAll('.gallery img');
+
+
+images.forEach(function(img) {
+  img.onclick = function() {
+    bigImage.src = this.src; 
+  }
+});
